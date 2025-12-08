b7817f2a56d855132d76bae882536ffdc80404e4
freefiresingapore24535-alt <freefiresingapore24535@gmail.com>
2025-12-08 15:37:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "22321078"))
 API_HASH = environ.get("API_HASH", "9960806d290cf4170e43355fcc3687ac")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8514251249:AAGdwc1jpJUB0PCWljHDm6R4m5oplN6ZAKI")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8575792576:AAGrl1vAuLQ1NlMyyul0OJ7EZ4Us9wv5FcQ")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
